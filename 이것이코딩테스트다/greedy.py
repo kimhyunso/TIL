564f53a2a927181e8432414b9242122d3d5c7209
kimhyunso <gustn9515@gmail.com>
2023-09-28 21:31:25 +0900

    20230928 21:31

---
+++ 이것이코딩테스트다/greedy.py
@@ -0,0 +1,108 @@
+# 현재 상황에서 당장 좋은 것만 고르는 것
+# 정당성 분석
+
+# 거스름돈 문제
+# 가장 큰 화폐부터 거술러주기
+N = 1260
+coins = [500, 100, 50, 10]
+
+
+result = 0
+
+
+for coin in coins:
+    result += N // coin
+    N = N % coin
+
+
+print(result)
+
+'''
+public class Main{
+    public static void main(String[] args){
+        int n = 1260;
+        int cnt = 0;
+        int[] coins = {500, 100, 50, 10};
+
+        for (int i = 0; i<conis.length; i++){
+            cnt += n / coins[i];
+            n = n % coins[i];
+        }
+
+        System.out.println(cnt);
+    }
+}
+'''
+
+
+# 1이 될때까지
+# 최대한 많이 나누기
+'''
+N, K = map(int, input().split())
+result = 0
+
+def divideCount(N):
+    count = 0
+    while N != 1:
+        N = N // K
+        count += 1
+    return count
+
+
+if N % K == 0:
+    result = divideCount(N)
+else:
+    N - 1
+    result += 1
+    result += divideCount(N)
+
+
+print(result)
+'''
+
+
+# N log N
+n, k = map(int, input().split())
+result = 0
+
+while True:
+    # N이 K로 나누어 떨어지는 수가 될 때까지 빼기
+    # 만약 N이 K로 나누어 떨어지지 않는다고 했을 때 가장 가까운 K로 나누어 떨어지는 수가 어떤건지를 찾고자 할 때 사용
+    # target K로 나누어 떨어지는 수
+    target = (n // k) * k
+
+    # 총 연산을 수행하는 횟수
+    # 1을 빼는 연산을 몇번 수행할지 한번에 더하는 것
+    result += (n - target)
+    # n이 target
+    n = target
+    
+    # N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
+        break
+    
+    # K로 나누기
+    result += 1
+    n //= k
+
+
+# 마지막으로 남은 수에 대하여 1씩 빼기
+result += (n-1)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
