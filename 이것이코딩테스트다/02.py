367b1365f75294021bbe9618ebdf32c7b6a09ba4
kimhyunso <gustn9515@gmail.com>
2023-09-28 19:31:28 +0900

    20230928 19:31

---
+++ 이것이코딩테스트다/02.py
@@ -0,0 +1,56 @@
+import sys
+
+n = int(input())
+data = list(map(int, input().split()))
+
+print(n)
+
+
+print(data)
+
+
+a, b, c = map(int, input().split())
+
+print(a, b, c)
+
+
+
+data = sys.stdin.readline().rstrip()
+print(data)
+
+
+
+a = 1
+b = 2
+
+print(a, b)
+print(7, end=' ')
+print(8, end=' ')
+
+answer = 7
+print(' 정답은 ' + str(answer) + '입니다.')
+
+
+
+answer = 7
+print(f'정답은 {answer} 입니다.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 이것이코딩테스트다/03.py
@@ -0,0 +1,63 @@
+x = 15
+
+if x >= 10:
+    print('x >= 10')
+
+if x >= 0:
+    print('x >= 0')
+
+if x >= 30:
+    print('x >= 30')
+
+
+score = 85
+
+if score >= 70:
+    print('성적이 70점 이상입니다.')
+    if score >= 90:
+        print('우수한 성적입니다.')
+
+else:
+    print('성적이 70점 미만입니다.')
+    print('조금 더 분발하세요')
+
+print('프로그램을 종료합니다.')
+
+
+a = 5
+
+if a >= 0:
+    print('a >= 0')
+elif a >= -10:
+    print('0 > a > -10')
+else:
+    print('-10 > a')
+
+
+
+
+score = 85
+
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+else:
+    print('F')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
