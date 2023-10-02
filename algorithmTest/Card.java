8b3b8cbd02bfadc46d83d7f2d4ec46a1e6332c8e
kimhyunso <gustn9515@gmail.com>
2023-10-02 14:49:17 +0900

    20231002 14:50

---
+++ algorithmTest/Card.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Card {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<Integer>();
+        int cardCount = sc.nextInt();
+        
+        
+        for (int i=1; i<=cardCount; i++){
+            queue.offer(i);
+        }
+
+        while(queue.size() != 1){
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.print(queue.peek());
+    }
+}
