9c4402e876c0cfff5fc01226a7377df68e731b5b
kimhyunso <gustn9515@gmail.com>
2023-10-02 21:11:45 +0900

    20231002 21:11

---
+++ algorithmTest/요세푸스문제.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+
+public class 요세푸스문제 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        String []numbers = br.readLine().split(" ");
+        int personCount = Integer.parseInt(numbers[0]);
+        int deleteCount = Integer.parseInt(numbers[1]);
+        int cnt = 1;
+
+        for (int i=1; i<=personCount; i++){
+            queue.offer(i);
+        }
+        System.out.print("<");
+        while (!queue.isEmpty()){
+            if (cnt++ % deleteCount == 0){
+                if (queue.size() != 1){
+                    System.out.print(queue.poll()+", ");
+                }else{
+                     System.out.print(queue.poll());
+                }
+                
+            }else{
+                queue.add(queue.poll());
+            }
+        }
+        System.out.print(">");
+
+    }
+    
+}
