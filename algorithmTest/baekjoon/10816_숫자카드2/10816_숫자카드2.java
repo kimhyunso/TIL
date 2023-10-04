334ebe0b249efbca4afbadeb555baa3f558c2522
kimhyunso <gustn9515@gmail.com>
2023-10-04 18:52:21 +0900

    20231004 18:52

--- algorithmTest/baekjoon/10816_숫자카드2/10816_숫자카드2.java
+++ algorithmTest/baekjoon/10816_숫자카드2/10816_숫자카드2.java
@@ -4,28 +4,72 @@ import java.io.*;
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        StringBuffer sb = new StringBuffer();
         int N = Integer.parseInt(br.readLine());
-        String []cardN = br.readLine().split(" ");
-        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int cardN[] = new int[N];
+        int cnt = 0;
+
+        while (st.hasMoreTokens()){
+            cardN[cnt++] = Integer.parseInt(st.nextToken());
+        }
 
       
         int M = Integer.parseInt(br.readLine());
-        String []cardM = br.readLine().split(" ");
-        
-        Arrays.sort(cardN);
-        Arrays.sort(cardM);
-       
+        st = new StringTokenizer(br.readLine(), " ");
+        int cardM[] = new int[M];
+        cnt = 0;
 
-        
-        for (int i=0; i<cardM.length; i++){
-
-            
-
+        while (st.hasMoreTokens()){
+            cardM[cnt++] = Integer.parseInt(st.nextToken());
         }
 
-        
+        Arrays.sort(cardN);
 
 
+
+        for (int i=0; i<cardM.length; i++){
+            int lowerBound = lowerBound(cardM[i], cardN);
+            int upperBound = upperBound(cardM[i], cardN);
+            sb.append((lowerBound + upperBound) + " ");
+        }
+
+        System.out.print(sb.toString());
     }
+
+
+    public static int lowerBound(int key, int arr[]){
+        int left = 0;
+        int right = arr.length - 1;
+        int mid = 0;
+
+        while(left < right){
+            mid = (left + right) / 2;
+            
+            if (key <= arr[mid])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+
+    public static int upperBound(int key, int arr[]){
+        int left = 0;
+        int right = arr.length - 1;
+        int mid = 0;
+
+        while (left < right){
+            mid = (left + right) / 2;
+
+            if (key < arr[mid])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+
 }
