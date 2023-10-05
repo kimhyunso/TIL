e2dc6d7178a54d10e2dc37ddae37f9b866677c7e
kimhyunso <gustn9515@gmail.com>
2023-10-05 18:00:13 +0900

    20231005 18:00

--- algorithmTest/자료구조/MyArrayList.java
+++ algorithmTest/자료구조/MyArrayList.java
@@ -1,3 +1,5 @@
+package 자료구조;
+
 import java.util.*;
 
 public class MyArrayList<E> implements MyList<E>{
@@ -47,11 +49,146 @@ public class MyArrayList<E> implements MyList<E>{
 
 
         // 들어있는 데이터가 하나도 없을 경우 (빈 배열)
-        if (element_capacity == 0)
+        if (Arrays.equals(elementData, EMPTY_ELEMENTDATA)){
+            elementData = new Object[DEFAULT_CAPACITY];
+            return;
+        }
+    }
+
+    @Override
+    public boolean add(Object value){
+        resize();
+
+        elementData[size++] = value;
+        return true;
 
     }
 
+    @Override
+    public void add(int index, Object value){
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
+
+        if (index == size)
+            add(value);
+        else{
+            resize();
+            for (int i=size; i>index; i--)
+                elementData[i] = elementData[i - 1];
+
+            elementData[index] = value;
+            size ++;
+        }
+    }
+
+    @Override
+    public int indexOf(Object value){
+
+        if (value == null){
+            for (int i=0; i<size; i++){
+                if (elementData[i] == null)
+                    return i;
+            }
+
+        }else{
+            for (int i=0; i<size; i++){
+                if (elementData[i].equals(value)){
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object value){
+        if (value == null){
+            for (int i=size; i >= 0; i--)
+                if (elementData[i] == null)
+                    return i;
+        }else{
+            for (int i=size; i>=0; i--)
+                if (elementData[i].equals(value))
+                    return i;
+        }
+        return -1;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public E remove(int index){
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+
+        E element = (E) elementData[index];
+        elementData[index] = null;
+
+        for (int i=index; i<size -1; i++){
+            elementData[i] = elementData[i + 1];
+            elementData[i + 1] = null;
+        }
+
+        size --;
+
+        resize();
+
+        return element;
+    }
 
 
+    @Override
+    public boolean remove(Object value){
+        int idx = indexOf(value);
 
+        if (idx == -1) return false;
+
+        remove(idx);
+
+        return true;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public E get(int index){
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+        return (E) elementData[index];
+    }
+
+
+    @Override
+    public void set(int index, Object value){
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+
+        elementData[index] = value;
+    }
+
+
+    @Override
+    public int size(){
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    @Override
+    public void clear(){
+        elementData = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    @Override
+    public boolean contains(Object value){
+        return indexOf(value) >= 0 ? true : false;
+    }
+
+    @Override
+    public String toString(){
+        return Arrays.toString(elementData);
+    }
 }
