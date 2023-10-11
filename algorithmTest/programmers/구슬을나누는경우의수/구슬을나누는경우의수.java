f29458c6cfd224dad837a41e58d98075aaecd813
kimhyunso <gustn9515@gmail.com>
2023-10-11 18:49:46 +0900

    20231011 18:50

--- algorithmTest/programmers/구슬을나누는경우의수/구슬을나누는경우의수.java
+++ algorithmTest/programmers/구슬을나누는경우의수/구슬을나누는경우의수.java
@@ -1,21 +1,41 @@
 package programmers.구슬을나누는경우의수;
-import java.util.*;
 
+/*
+ 머쓱이는 구슬을 친구들에게 나누어주려고 합니다. 
+ 구슬은 모두 다르게 생겼습니다. 
+ 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때, 
+ balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+ */
+
+ /*
+풀이법 : factorial 함수를 만들어
+BigInteger 사용 :: 무한대까지 저장 가능
+  */
+
+
+import java.math.*;
 public class 구슬을나누는경우의수 {
     public static void main(String[] args){
-        int balls = 3;
-        int share = 2;
+        int balls = 5;
+        int share = 3;
 
         solution(balls, share);
 
     }
 
-    public static int solution(int balls, int share){
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        for (int i=1; i<=balls; i++)
-            map.put(i, 1);
-
-        return 0;
+    public static BigInteger solution(int balls, int share){
+        return factorial(balls).divide(factorial(balls - share).multiply(factorial(share)));
     }
+
+    public static BigInteger factorial(int n){
+        BigInteger ret = new BigInteger("1");
+        BigInteger from = new BigInteger("1");
+        BigInteger to = new BigInteger(String.valueOf(n));
+
+        for (BigInteger i=from; i.compareTo(to) <= 0; i = i.add(BigInteger.ONE))
+            ret = ret.multiply(i);
+
+        return ret;
+    }
+    
 }
