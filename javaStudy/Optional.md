e405c8edb1fa97b145a72c2ae94432282708f18e
kimhyunso <gustn9515@gmail.com>
2023-10-14 18:19:01 +0900

    20231014 18:18

--- javaStudy/Optional.md
+++ javaStudy/Optional.md
@@ -29,22 +29,17 @@ null이 올 수 있는 값을 감싸는 Wrapper 클래스
 
 # 대체
 ```java
-
-// 비어있는 Optional 객체로 초기화
-Optional<String> age  = Optional.empty();
-
-// Optional.empty
-System.out.println(age);
-
-// false
-System.out.println(age.isPresent());
-
 // 값이 Null이 아닌 경우
 Optional<String> age = Optinal.of("23");
 
+// true
+System.out.println(age.isPresent());
 
 // 값이 Null일 수도, 아닐 수도 있는 경우
 Optional<String> age = Optional.ofNullable("23");
+
+// 비어있는 Optional 객체로 초기화 :: empty
+Optional<String> optional  = Optional.empty();
 
 // 값이 없다면 "0"을 리턴 :: 파라미터로 값을 받는다.
 String myAge = optional.orElse("0");
@@ -52,7 +47,7 @@ String myAge = optional.orElse("0");
 String myAge = optional.orElse(getAge());
 
 // 파라미터로 함수형 인터페이스(함수)를 받는다.
-String myAge = optional.orElseGet(this::getAge();
+String myAge = optional.orElseGet(this::getAge());
 
 public String getAge(){
     return "23";
--- javaStudy/OptionalTest.java
+++ javaStudy/OptionalTest.java
@@ -1,7 +1,48 @@
 package javaStudy;
 
-public class OptionalTest {
-    public static void main(String[] args){
-        
+import java.util.*;
+
+
+
+
+
+class ClassName{
+    private int age;
+    private String name;
+
+    public ClassName(int age, String name){
+        this.age = age;
+        this.name = name;
     }
+
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+}
+
+
+
+public class OptionalTest {
+
+    public static void main(String[] args){
+
+        Optional<Integer> optional = Optional.of(20);
+        System.out.println(optional.get());
+
+        Optional<String> ageOptional = Optional.empty();
+        String myAge = ageOptional.orElse("123");
+        System.out.println(myAge);
+
+        Optional<String> testOptional = Optional.ofNullable(null);
+        System.out.println(testOptional);
+
+        ClassName className = new ClassName(23, "kim");
+        Optional<String> classOptional = Optional.empty();
+        String name = classOptional.orElseGet(className::getName);
+        System.out.println(name);
+    }
+
 }
