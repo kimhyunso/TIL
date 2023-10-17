be46722a3cfa965b0691e98a1358f596c93d07cf
kimhyunso <gustn9515@gmail.com>
2023-10-17 20:06:08 +0900

    20231017 20:06

--- algorithmTest/BinarySearch.java
+++ algorithmTest/BinarySearch.java
@@ -1,21 +1,21 @@
 import java.util.*;
 
-public class BinarySearch {
+class Main  {
     public static void main(String[] args){
         int numbers[] = {1, 13, 25, 36, 19};
         Arrays.sort(numbers);
-        int target = 18;
+        int target = 38;
 
         System.out.println(binarySearch(target, numbers));
     }
 
     public static int binarySearch(int target, int numbers[]){
 
-         int left = 0;
-        int right = numbers.length;
+        int left = 0;
+        int right = numbers.length - 1;
         int mid = 0;
         while (left <= right){
-            mid = numbers.length / 2;
+            mid = (left + right) / 2;
             
             if (numbers[mid] == target)
                 return numbers[mid];
