5711349e99af665b84f59f16c8192fcb591d0418
kimhyunso <gustn9515@gmail.com>
2023-10-26 15:45:51 +0900

    20231026 15:45

--- algorithmTest/programmers/시저암호/Main.java
+++ algorithmTest/programmers/시저암호/Main.java
@@ -2,19 +2,35 @@ package programmers.시저암호;
 
 public class Main {
     public static void main(String[] args){
-
-
-
+        String str = "e F d A B G";
+        int n = 4;
+        System.out.println(solution(str, n));
     }
 
     public static String solution(String s, int n){
         String answer = "";
 
-
         for (int i=0; i<s.length(); i++){
+            char temp = s.charAt(i);
+            
+            if (temp == ' ')
+                answer += " ";
+            else{
+                if(temp >= 'a' && temp <= 'z'){
+                    if (temp + n > 'z')
+                        answer += (char) (temp-26+n);
+                    else
+                        answer += (char) (temp + n);
 
 
+                }else if (temp >= 'A' && temp <= 'Z'){
+                    if (temp + n > 'Z')
+                        answer += (char) (temp-26+n);
+                    else
+                        answer += (char) (temp + n);
 
+                }
+            }            
         }
 
         return answer;
