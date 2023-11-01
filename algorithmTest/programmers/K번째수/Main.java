1878c9e35d65897d380d27622e20b30219835916
kimhyunso <gustn9515@gmail.com>
2023-11-01 17:02:08 +0900

    20231101 17:02

--- algorithmTest/programmers/K번째수/Main.java
+++ algorithmTest/programmers/K번째수/Main.java
@@ -4,23 +4,28 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        // int[] array = {};
-        // solution(array,);
-
-        
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {
+            {2, 5, 3},
+            {4, 4, 1},
+            {1, 7, 3}
+        };
+        solution(array, commands);
     }
 
     public static int[] solution(int[] array, int[][] commands){
         int answer[] = new int[commands.length];
+        
         for (int i=0; i<commands.length; i++){
+            int temp[];
             int to = commands[i][0];
             int from = commands[i][1];
             int findIndex = commands[i][2];
 
-            array = Arrays.copyOfRange(array, to, from);
-            Arrays.sort(array);
+            temp = Arrays.copyOfRange(array, to-1, from);
+            Arrays.sort(temp);
 
-            answer[i] = array[findIndex-1];
+            answer[i] = temp[findIndex-1];
         }
 
 
