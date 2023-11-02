d9dce428ce6ee617a0085aeeb69b64ba3098bb4c
kimhyunso <gustn9515@gmail.com>
2023-11-02 11:07:57 +0900

    20231102 11:07

--- algorithmTest/programmers/수박수박수박수박수박수/Main.java
+++ algorithmTest/programmers/수박수박수박수박수박수/Main.java
@@ -1,5 +1,18 @@
 package programmers.수박수박수박수박수박수;
 
+/**
+ * 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 
+ * 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+ */
+
+/**
+ * 풀이법 :
+ * n만큼 반복
+ * 1. 짝수 일 경우 : answer에 "수"를 더함
+ * 2. 홀수 일 경우 : answer에 "박"을 더함
+ */
+
+
 public class Main {
     public static void main(String[] args){
         int n = 3;
--- algorithmTest/programmers/시저암호/Main.java
+++ algorithmTest/programmers/시저암호/Main.java
@@ -1,5 +1,26 @@
 package programmers.시저암호;
 
+
+/**
+ * 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 
+ * 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. 
+ * "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+ */
+
+/**
+ * 풀이법 : 
+ * 문자열 s를 `charAt()`을 사용하여 char로 분리 후, 분리된 char(temp)를 이용해 공백인지 체크
+ * 1. 공백일 경우 : answer에 공백 추가
+ * 2. 공백이 아닌 경우 : 
+ * 2-1. 소문자인 경우 : 
+ * 2-1-1. (temp + n)이 z보다 큰 경우 : (temp-26+n) 알파벳이 26개이기 때문에 temp에서 26을 빼고, n만큼을 더한다.
+ * 2-1-1. (temp + n)이 z보다 작은 경우 : (temp + n) temp를 n만큼 더한다.
+ * 2-2. 대문자인 경우 : 
+ * 2-2-1. (temp + n)이 Z보다 큰 경우 : (temp-26+n) 알파벳이 26개이기 때문에 temp에서 26을 빼고, n만큼을 더한다.
+ * 2-2-2. (temp + n)이 Z보다 작은 경우 : (temp + n) temp를 n만큼 더한다.
+ */
+
+
 public class Main {
     public static void main(String[] args){
         String str = "e F d A B G";
