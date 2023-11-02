2589c24409dea0e5503fe385da9c2fbb28f0b581
kimhyunso <gustn9515@gmail.com>
2023-11-02 10:44:37 +0900

    20231102 10:44

--- algorithmTest/programmers/문자열이몇번등장하는지세기/Main.java
+++ algorithmTest/programmers/문자열이몇번등장하는지세기/Main.java
@@ -1,26 +1,25 @@
 package programmers.문자열이몇번등장하는지세기;
 
-/*
-문자열 myString과 pat이 주어집니다.
-myString에서 pat이 등장하는 횟수를 return 하는 solution 함수를 완성해 주세요.
+/**
+ * 문자열 myString과 pat이 주어집니다.
+ * myString에서 pat이 등장하는 횟수를 return 하는 solution 함수를 완성해 주세요.
  */
 
-/*
-풀이법 : 
-myString = "banana", pat = "ana"
-
-1번째 루프
-0 ~ 3-1 [ban] : ana가 포함되어있는지 판단
-
-2번째 루프
-1 ~ 4-1 [ana] : ana가 포함되어있는지 판단
-
-3번째 루프
-2 ~ 5-1 [nan] : ana가 포함되어있는지 판단
-
-4번째 루프
-3 ~ 6-1 [ana] : ana가 포함되어있는지 판단
-루프 종료 끝까지 다비교함
+/**
+ * 풀이법 : 
+ * myString = "banana", pat = "ana"
+ * 1번째 루프
+ * 0 ~ 3-1 [ban] : ana가 포함되어있는지 판단
+ * 
+ * 2번째 루프
+ * 1 ~ 4-1 [ana] : ana가 포함되어있는지 판단
+ * 
+ * 3번째 루프
+ * 2 ~ 5-1 [nan] : ana가 포함되어있는지 판단
+ * 
+ * 4번째 루프
+ * 3 ~ 6-1 [ana] : ana가 포함되어있는지 판단
+ * 루프 종료 끝까지 다비교함
  */
 
 
