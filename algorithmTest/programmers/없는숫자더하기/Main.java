438f338320deecf4f885ae3e65de307d4ffedcff
kimhyunso <gustn9515@gmail.com>
2023-11-02 13:50:57 +0900

    20231102 13:50

--- algorithmTest/programmers/없는숫자더하기/Main.java
+++ algorithmTest/programmers/없는숫자더하기/Main.java
@@ -1,5 +1,17 @@
 package programmers.없는숫자더하기;
 
+/**
+ * 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. 
+ * numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+ */
+
+/**
+ * 풀이법 : 
+ * 0~9까지 더한다. (sum)
+ * answer에 배열로 들어온 모든 수들을 더한다.
+ * sum에서 answer를 뺀다.
+ */
+
 import java.util.*;
 
 public class Main {
@@ -12,7 +24,7 @@ public class Main {
     public static int solution(int[] numbers){
         int answer = -1;
         int sum = 0;
-        Arrays.sort(numbers);
+       
 
         for (int i=0; i<=9; i++){
             sum += i;
--- algorithmTest/programmers/옹알이/Main.java
+++ algorithmTest/programmers/옹알이/Main.java
@@ -1,8 +1,21 @@
 package programmers.옹알이;
+/**
+ * 머쓱이는 태어난 지 11개월 된 조카를 돌보고 있습니다. 
+ * 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음과 네 가지 발음을 조합해서 만들 수 있는 발음밖에 하지 못하고 
+ * 연속해서 같은 발음을 하는 것을 어려워합니다. 
+ * 문자열 배열 babbling이 매개변수로 주어질 때, 
+ * 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+ */
+
+/**
+  * 풀이법 : 
+  * 한 단어모음에서 중복된 단어가있다면 1로 바꾸고
+  * 중복된 단어가 없다면 " "로 바꾸어 trim(공백제거) 후 길이가 0일 경우 count를 증가시킨다.
+  *
+  */
 
 public class Main {
     public static void main(String[] args){
-
         String babbling[] = {"ayayeayaaya"};
 
         System.out.println(solution(babbling));
@@ -18,14 +31,8 @@ public class Main {
         for (int i=0; i<babbling.length; i++){
             String word = babbling[i];
             for (int j=0; j<words.length; j++){
-                int firstIndex = word.indexOf(words[j]);
-                int lastIndex = word.lastIndexOf(words[j]);
-
-                if (firstIndex + words[j].length() != lastIndex && firstIndex != -1){
-                    word = word.replace(duplicatedWord[j], "1");
-                    word = word.replace(words[j], " ");                    
-                }
-               
+                word = word.replace(duplicatedWord[j], "1");
+                word = word.replace(words[j], " ");                    
             }
 
             if (word.trim().length() == 0)
