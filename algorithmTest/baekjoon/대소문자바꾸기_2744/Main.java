3da2b9f5c9f0877d8d987cda48c5337ada2ba4f3
kimhyunso <gustn9515@gmail.com>
2023-11-10 17:37:21 +0900

    20231110 17:37

---
+++ algorithmTest/baekjoon/대소문자바꾸기_2744/Main.java
@@ -0,0 +1,42 @@
+package baekjoon.대소문자바꾸기_2744;
+
+/**
+ * 1. 아이디어
+ * - for문을 돌면서 소문자인지 대문자인지 확인 후
+ * - 소문자 : ('a' - 현재 문자) + 'A'
+ * - 대문자 : ('A' - 현재문자) + 'a'
+ * 2. 시간복잡도
+ * - 문자열 길이만큼, 최대길이 : 100
+ * - O(N) > 가능
+ * 3. 자료구조
+ * - 받은 문자열 String
+ * 
+ */
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        // char[] answer = line.toCharArray();
+        String answer = "";
+        String line = input.readLine();
+
+        for (int i=0; i<line.length(); i++){
+            char lineToChar = line.charAt(i);
+            // 소문자
+            if (lineToChar >= 'a' && lineToChar <= 'z'){
+                // 소문자에서 현재문자를 빼고 대문자를 더해줌
+                answer += (char)((lineToChar - 'a') + 'A');
+                
+            }
+            // 대문자
+            else if (lineToChar >= 'A' && lineToChar <= 'Z'){
+                // 대문자에서 현재문자를 빼고 소문자를 더해줌
+                answer += (char)((lineToChar - 'A') + 'a');
+            }
+        }
+        System.out.println(answer);
+    }
+    
+}
---
+++ algorithmTest/baekjoon/애너그램만들기_1919/Main.java
@@ -0,0 +1,64 @@
+package baekjoon.애너그램만들기_1919;
+
+import java.io.*;
+
+/**
+ * 1. 아이디어
+ * - 두번째 줄 string에 대해 for문을 돌면서 같은 문자열의 갯수를 센다.
+ * 2. 시간복잡도
+ * - O(N) -> 1000 > 가능
+ * 3. 자료구조
+ * - 첫번째 영단어 String
+ * - 두번째 영단어 String
+ */
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String A = input.readLine();
+        String B = input.readLine();
+        /*
+                                    //        a, b, c, d, e ...
+        int countA[] = new int[26]; // dared [1, 0, 0, 2, 1 ...]
+        int countB[] = new int[26];
+
+        for (int i=0; i<A.length(); i++){
+            countA[A.charAt(i) - 'a'] ++;
+        }
+
+        for (int i=0; i<B.length(); i++){
+            countB[B.charAt(i) - 'a']++;
+        }*/
+
+        int countA[] = getAlphabetCount(A);
+        int countB[] = getAlphabetCount(B);
+
+
+        int cnt = 0;
+        for (int i=0; i<countA.length; i++){
+            
+            cnt += Math.abs(countA[i] - countB[i]); 
+            /* if (countA[i] > countB[i]){
+                cnt += countA[i] - countB[i];
+            }else if (countB[i] > countA[i]){
+                cnt += countB[i] - countA[i];
+            }*/
+        }
+
+
+        System.out.println(cnt);
+    }
+
+    public static int[] getAlphabetCount(String str){
+        int[] count = new int[26];
+
+         for (int i=0; i<str.length(); i++){
+            count[str.charAt(i) - 'a'] ++;
+        }
+
+        return count;
+    }
+    
+}
